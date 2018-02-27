0f67432250100ac9d2399143edde4c48d28efe57
sambouzroud <sambouzroud@gmail.com>
2018-02-26 18:33:47 -0700

    last one

--- config/connection.js
+++ config/connection.js
@@ -5,18 +5,18 @@ var connection;
 if (process.env.JAWSDB_URL) {
 	connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-	connection = mysql.createConnection({
-		host: 'localhost',
-		user: 'root',
-		password: 'sam067581968',
-		database: 'burgers_db'
-	});
-};
+// 	connection = mysql.createConnection({
+// 		host: 'o3iyl77734b9n3tg.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+// 		user: 'root',
+// 		password: 'w2kwle9ombyv2c7b',
+// 		database: 'sponyzcxpuhfrj7k'
+// 	});
 
 
-var connection = mysql.createConnection({
+
+connection = mysql.createConnection({
 	host: "localhost",
-	port: 3306,
+	root: 3306,
 	user: "root",
 	password: "sam067581968",
 	database: "burgers_db" 
